c549906b30e83eb16e68bacf10ff53fe70cf5659
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2023-11-10 18:28:11 +0700

    sus

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/Simple-Vue-Calculator/' : '/',
-  build: {
-    rollupOptions: {
-      // Убедитесь, что расширения файлов, такие как SVG, не исключены
-      exclude: ['**/*.svg']
-    }
-  }
-}
+};
