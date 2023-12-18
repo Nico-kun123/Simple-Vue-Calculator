9704639d5d5c868bafe4768c69376f32392e58cc
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2023-12-18 16:52:06 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,16 +8,5 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   base: './',
-  build: {
-    rollupOptions: {
-      // Убедитесь, что расширения файлов, такие как SVG, не исключены
-      exclude: ['**/*.svg'],
-    },
-  },
 })
